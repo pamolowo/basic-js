e50bfb5d94a33829fcecd89b1143c974f218bd10
pamolowo <88985213+pamolowo@users.noreply.github.com>
2023-02-13 14:09:28 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let  a = 4;
+let  b = 5;
+
+const result = function(){
+    console.log(a * b);
+};
+result();
+
+let firstName = 'Pamela';
+let lastName = 'Olowojebutu';
+let track = 'Frontend Development';
+let skill =  'web development';
+let favoriteColor = 'blue';
+let countryIndependence = '1960';
+
+console.log(`My name is  ${firstName} ${lastName}, I am in the  ${track} with skills in  ${skill}
+my favorite color is ${favoriteColor}. I am from Nigeria and my country independence was ${countryIndependence}`)
